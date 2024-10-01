67f2a797d954bfa9d0a30f56fb2afbe4bdf0280a
Andrei Matveyeu <andreimu@axis.com>
2024-10-01 15:52:00 +0200

    black fix

--- setup.py
+++ setup.py
@@ -39,4 +39,4 @@ def local_scheme(version) -> str:
 
 
 if __name__ == "__main__":
-     setup(use_scm_version={"local_scheme": local_scheme, "version_scheme": version_scheme})
+    setup(use_scm_version={"local_scheme": local_scheme, "version_scheme": version_scheme})
